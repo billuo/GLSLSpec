4f77b928e26b2de293c8bcf47e4a5b2434211ae9
Zhen Luo <461652354@qq.com>
2018-10-17 16:01:28 +0800

    Cleaned up OpenGL::UniformBlock::UniformBlock()

--- include/OpenGL/Introspection.hpp
+++ include/OpenGL/Introspection.hpp
@@ -125,23 +125,32 @@ struct UniformBlock : public Resource {
     static constexpr size_t n_properties = countof(properties);
 
     UniformBlock(GLuint program, GLint index, GLchar* name, GLint* values) : Resource(index, name) {
+        //
+        // per block property
         binding = values[0];
         size = values[1];
-        const GLint n_uniforms = values[2];
-        uniforms.reserve(n_uniforms);
         for (size_t i = 0; i < MaxShaderStage; ++i) {
             referenced[i] = values[3 + i];
         }
-        GLenum active_variables = GL_ACTIVE_VARIABLES;
-        auto&& indices = std::make_unique<GLint[]>(n_uniforms);
-        GLint max_name_length;
-        glGetProgramResourceiv(program, interface, index, 1, &active_variables, n_uniforms, nullptr, indices.get());
-        glGetProgramInterfaceiv(program, Uniform::interface, GL_MAX_NAME_LENGTH, &max_name_length);
+        //
+        // prepare to iterate uniforms in the block
+        const GLint n_uniforms = values[2];
         const GLint n_props = Uniform::n_properties;
         const GLenum* props = Uniform::properties;
+        // array of uniform indices
+        GLenum active_variables = GL_ACTIVE_VARIABLES;
+        auto&& indices = std::make_unique<GLint[]>(n_uniforms);
+        glGetProgramResourceiv(program, interface, index, 1, &active_variables, n_uniforms, nullptr, indices.get());
+        // uniform name
+        GLint max_name_length;
+        glGetProgramInterfaceiv(program, Uniform::interface, GL_MAX_NAME_LENGTH, &max_name_length);
         auto&& name_buffer = std::make_unique<GLchar[]>(max_name_length);
+        // uniform properties
         auto&& value_buffer = std::make_unique<GLint[]>(n_props);
+        // iterate
+        uniforms.reserve(n_uniforms);
         for (GLint i = 0; i < n_uniforms; ++i) {
+            /// OPT simply fetch from interface GL_UNIFORM with known indices[] to avoid duplicate.
             glGetProgramResourceiv(program, Uniform::interface, indices[i], n_props, props, n_props, nullptr,
                                    value_buffer.get());
             glGetProgramResourceName(program, Uniform::interface, indices[i], max_name_length, nullptr,
@@ -149,6 +158,7 @@ struct UniformBlock : public Resource {
             uniforms.emplace_back(program, indices[i], name_buffer.get(), value_buffer.get());
         }
     }
+
     void dump() const {
         Resource::dump();
         fprintf(stderr, "binding=%d, size=%d, uniforms:\n", binding, size);
